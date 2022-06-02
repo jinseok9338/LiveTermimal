20fa77288d20a3678566bab261abb10e889d1584
jinseok9338 <jinseok9338@gmail.com>
2022-06-02 07:33:50 +0000

    Making UseRef

---
+++ card_game_engine/how the game works.md
@@ -0,0 +1,5 @@
+### One
+
+1. create a deck
+2. create a hand
+3. start with the player one always show a hand before choosing the card
--- card_game_front_end/src/main.rs
+++ card_game_front_end/src/main.rs
@@ -1,3 +1,33 @@
+mod components;
+
+use web_sys::HtmlInputElement;
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    let input_ref = use_node_ref();
+
+    let onclick = {
+        let input_ref = input_ref.clone();
+        move |_| {
+            if let Some(input) = input_ref.cast::<HtmlInputElement>() {
+                input.focus()
+            } else {
+                println!("ds")
+            }
+        }
+    };
+    html! {
+    <div
+      class="text-light-foreground dark:text-dark-foreground min-w-max text-xs md:min-w-full md:text-base"
+      onclick={onclick}
+    >
+      <main class="bg-light-background dark:bg-dark-background w-full h-full p-2">
+        <h1>{"Hello World"}</h1>
+      </main>
+    </div>}
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
