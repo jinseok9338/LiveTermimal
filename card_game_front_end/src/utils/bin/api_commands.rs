57f9aad8074cffd35a70da36a8266216fd5fe110
jinseok9338 <jinseok9338@gmail.com>
2022-06-04 12:04:46 +0000

    done

--- card_game_front_end/src/utils/bin/api_commands.rs
+++ card_game_front_end/src/utils/bin/api_commands.rs
@@ -3,43 +3,6 @@ use reqwest::Error;
 use crate::utils::api::{get_quotes, get_read_me, get_weather};
 use crate::utils::{api::get_projects, api_types::Projects};
 
-// // // List of commands that require API calls
-
-// import { getProjects } from '../api';
-// import { getQuote } from '../api';
-// import { getReadme } from '../api';
-// import { getWeather } from '../api';
-
-// export const projects = async (args: string[]): Promise<string> => {
-//   const projects = await getProjects();
-//   return projects
-//     .map(
-//       (repo) =>
-//         `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
-//     )
-//     .join('\n');
-// };
-
-// export const quote = async (args: string[]): Promise<string> => {
-//   const data = await getQuote();
-//   return data.quote;
-// };
-
-// export const readme = async (args: string[]): Promise<string> => {
-//   const readme = await getReadme();
-//   return `Opening GitHub README...\n
-//   ${readme}`;
-// };
-
-// export const weather = async (args: string[]): Promise<string> => {
-//   const city = args.join('+');
-//   if (!city) {
-//     return 'Usage: weather [city]. Example: weather casablanca';
-//   }
-//   const weather = await getWeather(city);
-//   return weather;
-// };
-
 pub async fn projects(args: Vec<String>) -> Result<String, Error> {
     let response = get_projects().await.unwrap();
     let projects = response.json::<Vec<Projects>>().await?;
