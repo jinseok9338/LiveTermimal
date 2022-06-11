bc4ce8947d3fccfdbacc49b830bc567d6c8e7841
jinseok9338 <jinseok9338@gmail.com>
2022-06-11 13:31:01 +0000

    Font problem... maybe later ...

--- card_game_front_end/src/main.rs
+++ card_game_front_end/src/main.rs
@@ -27,7 +27,7 @@ fn app() -> Html {
         <HistoryContextProvider>
         <html class="dark" id="theme"> // for manually changing the dark theme and light theme
           <div
-                class="text-light-foreground dark:text-dark-foreground min-w-max text-xs md:min-w-full md:text-base transition duration-300"
+                class="text-light-foreground dark:text-dark-foreground min-w-max min-h-max text-xs md:min-w-full md:min-h-full md:text-base transition duration-300"
                 onclick={onclick}
               >
               <main class="bg-light-background dark:bg-dark-background w-full h-full p-2 transition duration-300" >
